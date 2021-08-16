6b9f084c85996d90c1a54338c56f465ca420e47d
Matheus Gomes <vsgmatheus@gmail.com>
2021-08-16 20:40:13 -0300

    Auditing data

--- backend/src/main/java/com/gomesmatheus/dscatalog/entities/Category.java
+++ backend/src/main/java/com/gomesmatheus/dscatalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.gomesmatheus.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -18,11 +22,27 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
 	public Category() {}
 
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	public Long getId() {
@@ -37,7 +57,13 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
