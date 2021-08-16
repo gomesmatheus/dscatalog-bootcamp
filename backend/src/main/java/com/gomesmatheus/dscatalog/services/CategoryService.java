eda7e9e0c43313703ddbb2e76c141b5abf0e98fb
Matheus Gomes <vsgmatheus@gmail.com>
2021-08-16 20:23:57 -0300

    Delete category

--- backend/src/main/java/com/gomesmatheus/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/gomesmatheus/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gomesmatheus.dscatalog.dto.CategoryDTO;
 import com.gomesmatheus.dscatalog.entities.Category;
 import com.gomesmatheus.dscatalog.repositories.CategoryRepository;
+import com.gomesmatheus.dscatalog.services.exceptions.DatabaseException;
 import com.gomesmatheus.dscatalog.services.exceptions.ResourcecNotFoundException;
 
 @Service
@@ -51,6 +54,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourcecNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourcecNotFoundException("Id not found " + id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 	
