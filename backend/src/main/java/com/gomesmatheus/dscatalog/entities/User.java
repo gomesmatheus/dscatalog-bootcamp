fc7ddc83c1fa203f40dd77b448e677f3548b3026
Matheus Gomes <vsgmatheus@gmail.com>
2021-09-27 10:44:47 -0300

    kit

--- backend/src/main/java/com/gomesmatheus/dscatalog/entities/User.java
+++ backend/src/main/java/com/gomesmatheus/dscatalog/entities/User.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,13 +28,13 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
-	private User() {}
+	public User() {}
 	
 	public User(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
